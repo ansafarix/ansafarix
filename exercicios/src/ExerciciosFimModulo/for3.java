80b554d614d9b9a46f332ac06b902eb169123cef
Anderson Safariz <ansafarix@gmail.com>
2021-06-18 01:26:11 -0300

    vb
    vs

---
+++ exercicios/src/ExerciciosFimModulo/for3.java
@@ -0,0 +1,11 @@
+package ExerciciosFimModulo;
+
+public class for3{
+public static void main(String[] args) {
+    
+    for (String v = "#"; !v.equals("#####"); v+="#") {
+        System.out.println(v);
+        
+    }
+}
+}
